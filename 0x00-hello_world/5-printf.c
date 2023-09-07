a5145fd050352f57b3b32decba6bf39ab14445b5
Rodaina Mansi <rodainamansi55@gmail.com>
2023-09-07 11:18:56 -0700

    ccc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: program that prints with printf function
+ * return(0) sucess
+*/
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
